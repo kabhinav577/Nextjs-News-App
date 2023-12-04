c16506205da4680df12b5e2d43f939249d0df035
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-12-04 20:39:19 +0530

    Using dotenv for Protecting API data

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,17 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import { getAuth } from 'firebase/auth';
+// console.log(typeof process.env.FIREBASE_API_KEY);
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: 'nextjs-news-app.firebaseapp.com',
+//   projectId: 'nextjs-news-app',
+//   storageBucket: 'nextjs-news-app.appspot.com',
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdtAfSFE877nl5533pwZ3YG7KKJd85U8k',
-  authDomain: 'nextjs-news-app.firebaseapp.com',
-  projectId: 'nextjs-news-app',
-  storageBucket: 'nextjs-news-app.appspot.com',
-  messagingSenderId: '716542056951',
-  appId: '1:716542056951:web:2e76af7b7b78ff68ae36f2',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
